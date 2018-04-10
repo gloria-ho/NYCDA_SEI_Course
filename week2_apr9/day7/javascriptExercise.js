bfc1db4b6b956e40ccbe9abe5c180264719906fd
silverdragonia <silverdragonia@gmail.com>
2018-04-10 12:26:22 -0400

    updating files

--- week2_apr9/day7/javascriptExercise.js
+++ week2_apr9/day7/javascriptExercise.js
@@ -1,10 +1,50 @@
+// Exercise #1
 alert('Hello World from .jS');
 
+// Exercise #2
 const name = 'Gloria';
 let myAge = '31';
 
+// via console.log
 console.log(`My name is ${name} and I am ${myAge} years old.`);
 
+// via calling a function
 function printMyInfo() {
+	const name = 'Gloria';
+	let myAge = '31';
 	console.log(`My name is ${name} and I am ${myAge} years old.`);
+}
+
+// Exercise #3
+let mathOne = 20;
+let mathTwo = 3;
+
+function exer3() {
+	console.log(mathSum = mathOne + mathTwo);
+	console.log(mathProduct = mathOne * mathTwo);
+}
+
+// Exercise #4
+
+//via console.log
+let classmates = ['Q', 'Al', 'Eddie', 'Iryna', 'Sharron'];
+console.log(classmates[3]);
+
+let multiDim = [
+	['hello','goodbye','food'],
+	['something','else','here'],
+	['beans','fruit','veggies']
+];
+console.log(multiDim[2][0]);
+
+//via calling a function
+function exer4() {
+	let classmates = ['Q', 'Al', 'Eddie', 'Iryna', 'Sharron'];
+	console.log(classmates[3]);
+	let multiDim = [
+		['hello','goodbye','food'],
+		['something','else','here'],
+		['beans','fruit','veggies']
+	];
+	console.log(multiDim[2][0]);
 }
