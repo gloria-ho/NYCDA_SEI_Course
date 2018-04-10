256aedce5fd4ad4c255149174bb6fb7474ee6fb0
silverdragonia <silverdragonia@gmail.com>
2018-04-10 11:46:41 -0400

    adding day7

---
+++ week2_apr9/day7/javascriptExercise.js
@@ -0,0 +1,10 @@
+alert('Hello World from .jS');
+
+const name = 'Gloria';
+let myAge = '31';
+
+console.log(`My name is ${name} and I am ${myAge} years old.`);
+
+function printMyInfo() {
+	console.log(`My name is ${name} and I am ${myAge} years old.`);
+}
