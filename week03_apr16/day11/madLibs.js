5edf046ee920b7ae2182ae8552bf4b60731a8d95
silverdragonia <silverdragonia@gmail.com>
2018-04-17 14:23:49 -0400

    updating files

--- week03_apr16/day11/madLibs.js
+++ week03_apr16/day11/madLibs.js
@@ -11,19 +11,30 @@ let myTarget3 = document.querySelectorAll('.subject');
 let submit = document.querySelector('#submit');
 let content = document.querySelector('#content');
 
+
+function replaceText(input, target) {
+	for (i = 0; i < target.length; i++) {
+		target[i].innerHTML = input.value;
+	}
+}
+
 submit.addEventListener('click', function() {
 	content.style.display = 'block';
 
-	for (i = 0; i < myTarget1.length; i++) {
-		myTarget1[i].innerHTML = myInput1.value;
-	}
+	// for (i = 0; i < myTarget1.length; i++) {
+	// 	myTarget1[i].innerHTML = myInput1.value;
+	// }
 
-	for (i = 0; i < myTarget2.length; i++) {
-		myTarget2[i].innerHTML = myInput2.value;
-	}
+	// for (i = 0; i < myTarget2.length; i++) {
+	// 	myTarget2[i].innerHTML = myInput2.value;
+	// }
 
-	for (i = 0; i < myTarget3.length; i++) {
-		myTarget3[i].innerHTML = myInput3.value;
-	}
+	// for (i = 0; i < myTarget3.length; i++) {
+	// 	myTarget3[i].innerHTML = myInput3.value;
+	// }
+
+	replaceText(myInput1, myTarget1);
+	replaceText(myInput2, myTarget2);
+	replaceText(myInput3, myTarget3);
 
 });
