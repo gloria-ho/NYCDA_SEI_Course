3c2714037d94a748e877e8577e0d673d91f7a09d
silverdragonia <silverdragonia@gmail.com>
2018-04-17 12:37:53 -0400

    updating files

--- week03_apr16/day11/madLibs.js
+++ week03_apr16/day11/madLibs.js
@@ -1,29 +1,29 @@
 let myInput1 = document.querySelector('#myAdj1');
-let myTarget1 = document.querySelector('.adjective1');
+let myTarget1 = document.querySelectorAll('.adjective1');
 
 let myInput2 = document.querySelector('#myAdj2');
 let myTarget2 = document.querySelectorAll('.adjective2');
 
 let myInput3 = document.querySelector('#mySub');
-let myTarget3 = document.querySelector('.subject');
+let myTarget3 = document.querySelectorAll('.subject');
 
 
 let submit = document.querySelector('#submit');
-
 let content = document.querySelector('#content');
-
-console.log(myInput2);
-console.log(myTarget2);
-
 
 submit.addEventListener('click', function() {
 	content.style.display = 'block';
 
-	myTarget1.innerHTML = myInput1.value;
+	for (i = 0; i < myTarget1.length; i++) {
+		myTarget1[i].innerHTML = myInput1.value;
+	}
 
 	for (i = 0; i < myTarget2.length; i++) {
 		myTarget2[i].innerHTML = myInput2.value;
 	}
 
-	myTarget3.innerHTML = myInput3.value;
+	for (i = 0; i < myTarget3.length; i++) {
+		myTarget3[i].innerHTML = myInput3.value;
+	}
+
 });
