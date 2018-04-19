72c5293c44dc990bf93e85db9d633cd83d14a03c
silverdragonia <silverdragonia@gmail.com>
2018-04-19 10:59:32 -0400

    updating files

--- week03_apr16/day14/warmUp.js
+++ week03_apr16/day14/warmUp.js
@@ -1,8 +1,19 @@
 let button = document.querySelector('#button');
 let img = document.querySelector('img');
+let isHidden = false;
 
 button.addEventListener('click', function() {
-	img.classList.toggle('hidden');
-	if (this.innerText === 'Show image') this.innerText = 'Hide image';
-	else this.innerText = 'Show image';
+	// img.classList.toggle('hidden');
+	// if (this.innerText === 'Show image') this.innerText = 'Hide image';
+	// else this.innerText = 'Show image';
+	if (isHidden) {
+		img.classList.remove('hidden');
+		button.innerText = 'Hide image';
+		isHidden = false;
+	} else {
+		img.classList.add('hidden');
+		button.innerText = 'Show image';
+		isHidden = true;
+	}
+
 })
