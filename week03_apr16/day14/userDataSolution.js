49d5872e3e2502611abf6dddbb5d63c46b8a30e5
silverdragonia <silverdragonia@gmail.com>
2018-04-20 21:19:09 -0400

    updating files

---
+++ week03_apr16/day14/userDataSolution.js
@@ -0,0 +1,82 @@
+// define the Student class
+class Student {
+    constructor(myFirstName, myLastName) {
+        // define the properties the student will have
+        this.firstName = myFirstName;
+        this.lastName = myLastName;
+        this.assignmentScores = [];
+        this.averageScore = 0;
+        this.topScore = 0;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    addScore(score) {
+        // add the new score to the array
+        this.assignmentScores.push(score);
+
+        // call the function to update the average and topScore
+        this.updateAverageAndTopScore();
+    }
+
+    removeScore(scoreToRemove) {
+        // find the index of the score to remove
+        let indexToRemove = this.assignmentScores.indexOf(scoreToRemove);
+        // call the splice method passing in the index
+        this.assignmentScores.splice(indexToRemove, 1);
+
+        // call the function to update the average and topScore
+        this.updateAverageAndTopScore();
+    }
+
+    updateAverageAndTopScore() {
+        // declare variables to hold the running sum and highest score
+        let sum = 0;
+        let highestScore = this.assignmentScores[0];
+
+        for (let i = 0; i < this.assignmentScores.length; i++) {
+            // in the for loop we will add the sum and check the highest value
+            sum = sum + this.assignmentScores[i];
+            if (this.assignmentScores[i] > highestScore) {
+                highestScore = this.assignmentScores[i];
+            }
+        }
+
+        // update the average score and topScore after the loop finishes.
+        this.averageScore = sum / this.assignmentScores.length;
+        this.topScore = highestScore;
+    }
+}
+
+let firstNameInput = document.getElementById('first-name');
+let lastNameInput = document.getElementById('last-name');
+let scoreInput = document.getElementById('score-input');
+let scoreButton = document.getElementById('score-button');
+
+let fullNameElement = document.getElementById('full-name');
+let averageElement = document.getElementById('student-average');
+let maxScoreElement = document.getElementById('student-max');
+
+let myStudent = new Student('', '')
+
+firstNameInput.addEventListener('keyup', function() {
+    myStudent.firstName = firstNameInput.value;
+    fullNameElement.innerText = myStudent.getFullName();
+});
+
+lastNameInput.addEventListener('keyup', function() {
+    myStudent.lastName = lastNameInput.value;
+    fullNameElement.innerText = myStudent.getFullName();
+});
+
+scoreButton.addEventListener('click', function() {
+    let scoreToAdd = Number(scoreInput.value);
+    myStudent.addScore(scoreToAdd);
+
+    averageElement.innerText = myStudent.averageScore;
+    maxScoreElement.innerText = myStudent.topScore;
+
+    scoreInput.value = '';
+});
