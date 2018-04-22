731229fa37d0028e6750cbd85f8ccc8d24596478
silverdragonia <silverdragonia@gmail.com>
2018-04-22 11:40:55 -0400

    updating files

--- week03_apr16/day15/randomCatGallery.js
+++ week03_apr16/day15/randomCatGallery.js
@@ -1,4 +1,16 @@
 //This comes from data.js
 let gifs = window.data.data;
 
-console.log(gifs.length);
+let randomButton = document.querySelector('#randomButton');
+let gifImg = document.querySelector('#gifImg');
+
+
+randomButton.addEventListener('click', function() {
+	let index = Math.floor(Math.random() * gifs.length);
+		if (!gifs[index]) {
+			console.error(`index ${index} doesn't exist`, gifs);
+		} else {
+			gifImg.src = gifs[index].images.original.url;
+			gifs.splice(index, 1);
+	}
+})
