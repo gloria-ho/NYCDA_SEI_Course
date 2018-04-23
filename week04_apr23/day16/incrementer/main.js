fa819509fe5ed23f79909f4b07f0275270bc151d
silverdragonia <silverdragonia@gmail.com>
2018-04-23 14:07:43 -0400

    updating files

--- week04_apr23/day16/incrementer/main.js
+++ week04_apr23/day16/incrementer/main.js
@@ -27,3 +27,36 @@ minusButton.addEventListener('click', function() {
 	myIncrementor.decrement();
 	number.innerText = myIncrementor.value;
 });
+
+
+/*
+class Incrementor {
+    constructor(value) {
+        this.value = value;
+    }
+
+    increment() {
+        this.value++;
+    }
+
+    decrement() {
+        this.value--;
+    }
+}
+
+let incrButton = document.querySelector('#incr-button');
+let decrButton = document.querySelector('#decr-button');
+let valueElem = document.querySelector('#value');
+
+let myIncrementor = new Incrementor(0);
+
+incrButton.addEventListener('click', function() {
+    myIncrementor.increment();
+    valueElem.innerText = myIncrementor.value;
+});
+
+decrButton.addEventListener('click', function() {
+    myIncrementor.decrement();
+    valueElem.innerText = myIncrementor.value;
+})
+*/
