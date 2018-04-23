c55d7d408392db4c391ab629741b313f19303954
silverdragonia <silverdragonia@gmail.com>
2018-04-23 17:40:35 -0400

    updating files

--- week03_apr16/day15/randomCatGallery/main.js
+++ week03_apr16/day15/randomCatGallery/main.js
@@ -1,13 +1,10 @@
-//This comes from data.js
 let gifs = window.data.data;
 
 let randomButton = document.querySelector('#randomButton');
 let gifImg = document.querySelector('#gifImg');
 
-
 randomButton.addEventListener('click', function() {
 	let index = Math.floor(Math.random() * gifs.length);
 	gifImg.src = gifs[index].images.original.url;
 	gifs.splice(index, 1);
-
 })
