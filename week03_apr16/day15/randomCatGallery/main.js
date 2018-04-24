94cab42e70a11bd1e957451b424e6fef97d5ee94
silverdragonia <silverdragonia@gmail.com>
2018-04-23 22:25:38 -0400

    updating files

--- week03_apr16/day15/randomCatGallery/main.js
+++ week03_apr16/day15/randomCatGallery/main.js
@@ -1,10 +1,55 @@
+// let gifs = window.data.data;
+
+// let randomButton = document.querySelector('#randomButton');
+// let gifImg = document.querySelector('#gifImg');
+
+// randomButton.addEventListener('click', function() {
+// 		let index = Math.floor(Math.random() * gifs.length);
+// 		gifImg.src = gifs[index].images.original.url;
+// 		gifs.splice(index, 1);
+// });
+
 let gifs = window.data.data;
 
-let randomButton = document.querySelector('#randomButton');
-let gifImg = document.querySelector('#gifImg');
+class RandomObjectProcessor {
+	constructor(arrayOfObjects) {
+		this.arrayOfObjects = arrayOfObjects;
+		this.alreadyUsed = [];
+	}
 
-randomButton.addEventListener('click', function() {
-	let index = Math.floor(Math.random() * gifs.length);
-	gifImg.src = gifs[index].images.original.url;
-	gifs.splice(index, 1);
-});
+	getARandomObject() {
+		let randomIndex = Math.floor(Math.random() * gifs.length);
+		return this.arrayOfObjects[randomIndex];
+	}
+
+	getARandomObjectNoRepeats() {
+		let numberHasAlreadyBeenUsed = true;
+		let randomObject = undefined;
+		while(numberHasAlreadyBeenUsed == true) {
+			let randomIndex = Math.floor(Math.random() * gifs.length);
+
+			let result = this.alreadyUsed.indexOf(randomIndex);
+			if (result < 0) {
+				this.alreadyUsed.push(randomIndex);
+				randomObject = gifs[randomIndex];
+				numberHasAlreadyBeenUsed = false;
+
+				if (this.alreadyUsed.length == gifs.length) {
+					this.alreadyUsed = [];
+				}
+			}
+		}
+
+		return randomObject;
+	}
+}
+
+let button = document.querySelector('#randomButton');
+let imgTag = document.querySelector('#gifImg');
+let myProcessor = new RandomObjectProcessor(gifs);
+
+button.addEventListener('click', function () {
+	let randomObject = myProcessor.getARandomObjectNoRepeats();
+	let newSrc = randomObject.images.original.url
+	imgTag.src = newSrc;
+})
