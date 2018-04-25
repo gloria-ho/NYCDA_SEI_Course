1fd8cb3198ba6264ebe2c1c10df7df0f04eee260
silverdragonia <silverdragonia@gmail.com>
2018-04-24 22:05:37 -0400

    updating files

--- assignments/personalPokedex/main.js
+++ assignments/personalPokedex/main.js
@@ -16,14 +16,35 @@ class Pokemon {
 		this.defense = 0;
 		this.abilities = [];
 
-		console.log(this.abilities);
+		let loadPromise = axios.get('https://pokeapi.co/api/v2/pokemon/' + this.id + '/')
+		.then(function(result) {
 
-		$.ajax({url: 'https://pokeapi.co/api/v2/pokemon/' + this.id + '/', success: this.loadData
-		});		
+			console.log(result.data.stats[5].base_stat);
+		});
+
+
+		// .then(function(result) {
+		// 	console.log(result);
+		// 	console.log(this);
+			// this.loadData(result);
+		// });
+
+		// $.ajax({url: 'https://pokeapi.co/api/v2/pokemon/' + this.id + '/', success: function(data) {
+		// 	this.hp = data.stats[5].base_stat;
+		// 	this.attack = data.stats[4].base_stat;
+		// 	this.defense = data.stats[3].base_stat;
+		// 	console.log(this.hp);
+			// for (let i = 0; i < data.abilities.length; i++) {
+			// 	this.abilities.push(data.abilities[i].ability.name)
+			// }
+		// }});
 	}
 
 	loadData(data) {
-		this.hp = data.stats[5].base_stat;
+		// console.log(data)
+		// console.log(this);
+		// console.log();
+		// this.hp = data.data.stats[5].base_stat;
 		// this.attack = data.stats[4].base_stat;
 		// this.defense = data.stats[3].base_stat;
 
@@ -31,7 +52,7 @@ class Pokemon {
 		// 	this.abilities.push(data.abilities[i].ability.name);
 		// }
 
-		console.log('this should print too ' + this.hp);
+		// console.log('this should print too ' + this.hp);
 		// console.log(this.hp, this.attack);
 	}
 
@@ -56,10 +77,11 @@ class Pokemon {
 };
 
 
-let bulbasaur = new Pokemon('baulbasaur', 1);
+let bulbasaur = new Pokemon('bulbasaur', 1);
 // let charmander = new Pokemon('charmander');
 // let squirtle = new Pokemon('squirtle');
 
-// PROBLEM : below console.log runs BEFORE loadData!! HP still = 0
 
-console.log(bulbasaur);
+
+console.log(bulbasaur.getHp());
+
--- week04_apr23/day17/jsonWorkshop/main.js
+++ week04_apr23/day17/jsonWorkshop/main.js
@@ -1,38 +1,24 @@
-let displayTest = document.querySelector('#displayTest');
-let displayTestImg = document.querySelector('#displayTestImg');
-
-
 let myPromise = axios.get('https://www.redbullshopus.com/products.json')
 .then(function(myResponse) {
-
-
-	console.log(myResponse.data.products);
+	// console.log(myResponse.data.products);
 
 	for (let i = 0; i < myResponse.data.products.length; i++) {
-
 		// make a new div with 3 children
 		let newDiv = document.createElement('div');
 		let newTitle = document.createElement('p');
 		let newBody = document.createElement('p');
 		let newImg = document.createElement('img');
+		// nest the children within the new div
 		newDiv.append(newTitle, newBody, newImg);
-
-		let currentDiv = document.querySelector('#div1');
-
+		// set the existing div
+		let currentDiv = document.querySelector('#emptyDiv');
 		// insert the new div
 		document.body.insertBefore(newDiv, currentDiv);
-
 		// inner.text title text
 		newTitle.innerText = myResponse.data.products[i].title;
-
 		// inner.html body description html
 		newBody.innerHTML = myResponse.data.products[i].body_html;
-		
 		// insert src image, need a loop for more than 1 img?? [i]..+1 
 		newImg.src = myResponse.data.products[i].images[0].src;
 	}
 })
-
-
-
-
