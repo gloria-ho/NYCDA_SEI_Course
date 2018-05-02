a9a090ec4e2e6b05e577311326f2302d68791a2c
silverdragonia <silverdragonia@gmail.com>
2018-05-02 12:18:15 -0400

    updating files

--- week05_apr30/day21/workshop/solution.js
+++
@@ -1,7 +0,0 @@
-let themeSelect = $('#theme-choice');
-
-themeSelect.change = function() {
-	document.body.classList.remote('morning', 'afternoon', 'night');
-	console.log(themeSelect.value);
-	console.log('test')
-};
