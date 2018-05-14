d03045840a6bc8728580aeca6e86ba6eba1e5297
silverdragonia <silverdragonia@gmail.com>
2018-05-14 14:42:12 -0400

    moved class out of ruby_petshop.rb, initial commit on ruby_petshope_queue.rb

--- week06_may07/day29/ruby_petshop.rb
+++ week06_may07/day29/ruby_petshop.rb
@@ -1,39 +1,4 @@
-class PetshopQueue
-  attr_reader :queue
-  def initialize
-    @queue = []
-    @tickets = 0
-  end
-
-  def dog_arrived
-    @tickets += 1
-    my_hash = {
-      'animal' => 'dog',
-      'ticket' => @tickets
-    }
-    @queue.push(my_hash)
-    puts "Your dog is queued with ticket\#: #{@tickets}"
-  end
-
-  def cat_arrived
-    @tickets += 1
-    my_hash = {
-      'animal' => 'cat',
-      'ticket' => @tickets
-    }
-    @queue.push(my_hash)
-    puts "Your cat is queued with ticket\#: #{@tickets}"
-  end
-
-  def completed
-    puts "Your #{@queue[0]['animal']} with ticket number #{@queue[0]['ticket']} has now been served"
-    @queue.shift
-  end
-
-  def quit
-    puts 'Goodbye.'
-  end
-end
+require './ruby_petshop_queue'
 
 petshop = "Please Select an Action:
   1. Dog Arrived
@@ -51,24 +16,30 @@ while response != 4 do
     queue.dog_arrived
     puts petshop
     response = gets.strip.to_i
+
   elsif response == 2
     queue.cat_arrived
     puts petshop
     response = gets.strip.to_i
+
   elsif response == 3
     if queue.queue.length == 0
       puts "~! Error: Please add some animals to the queue."
       puts petshop
       response = gets.strip.to_i
+
     else
     queue.completed
     puts petshop
     response = gets.strip.to_i
+
     end
+
   else
     p "~! Error: Please input a valid selection:"
     puts petshop
     response = gets.strip.to_i
+  
   end
 end
 
