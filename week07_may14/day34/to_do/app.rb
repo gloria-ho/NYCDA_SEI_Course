8835fae3e4538599ceeac9a9533c89cbd45f4a4e
silverdragonia <silverdragonia@gmail.com>
2018-05-21 12:10:00 -0400

    all features working without using index

--- week07_may14/day34/to_do/app.rb
+++ week07_may14/day34/to_do/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './Todo'
 
 current_todos = []
 current_id = 1
@@ -13,10 +14,12 @@ get ('/create_new') do
 end
 
 post ('/post_new') do
+  # id = current_id,
+  # new_post = Todo.new(params,id)
   new_post = {
     :id => current_id,
     :title => params[:item_title],
-    :descr => params[:to_do_descr]
+    :descr => params[:item_descr]
   }
   current_id += 1
   current_todos.push(new_post)
@@ -30,7 +33,7 @@ end
 
 post ('/update_post/:current_id') do
   @target_todo = current_todos.detect{|item| item[:id] == params[:current_id].to_i}
-  @target_todo[0][:title] = params[:item_title]
-  @target_todo[0][:descr] = params[:item_descr]
+  @target_todo[:title] = params[:item_title]
+  @target_todo[:descr] = params[:item_descr]
   redirect "/"
 end
