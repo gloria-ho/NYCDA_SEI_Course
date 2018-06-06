aaaef0c9af315ab6d3a36dcab4a182309dec35ec
Gloria H <silverdragonia@gmail.com>
2018-06-06 12:12:37 -0400

    updating comments

--- week10_june04/day46/rails_todos/app/assets/javascripts/application.js
+++ week10_june04/day46/rails_todos/app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+  // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -11,4 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//
 //= require_tree .
+// this requires ALL files in CURRENT DIRECTORY
+//
+//= require todos
