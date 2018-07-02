3801d9bfd6caaa382fa7710901d338d0f0c50425
Gloria H <silverdragonia@gmail.com>
2018-07-02 11:00:30 -0400

    updating test files

--- week10_june04/day46/rails_todos/app/models/todo.rb
+++ week10_june04/day46/rails_todos/app/models/todo.rb
@@ -3,4 +3,7 @@ class Todo < ApplicationRecord
   validates :name, :description, presence: true
   validates :name, length: { minimum: 4, maximum: 100}
   validates :priority, inclusion: { in: ['H', 'M', 'L'], message: "Please select a valid priority"}
+
+  scope :created_after, -> (my_time) {where("created_at >= ?", my_time) }
+  scope :high_priority, -> {where (priority: "H")}
 end
--- week10_june04/day46/rails_todos/test/models/todo_test.rb
+++ week10_june04/day46/rails_todos/test/models/todo_test.rb
@@ -4,4 +4,15 @@ class TodoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "todos without prioritiy should fail" do
+    my_todo = Todo.create(name: "123345", description: "dedbawbdl")
+
+    assert_not(my_todo.persisted?)
+  end
+
+  test "todos with invalid prioritiy should fail" do
+    my_todo = Todo.create(name: "123345", description: "dedbawbdl", priority: "W")
+
+    assert_not(my_todo.persisted?)
+  end
 end
